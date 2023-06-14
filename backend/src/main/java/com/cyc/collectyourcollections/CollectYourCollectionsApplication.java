7fbe9719af1d0d9c79150dd99ae54dda8898e1c9
VeronikaG97 <109737441+VeronikaG97@users.noreply.github.com>
2023-06-14 15:08:40 +0200

    setup front- and backend
    created frontend with react vite and backend is spring with web dependency

---
+++ backend/src/main/java/com/cyc/collectyourcollections/CollectYourCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.cyc.collectyourcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectYourCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectYourCollectionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cyc/collectyourcollections/CollectYourCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyc.collectyourcollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectYourCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
